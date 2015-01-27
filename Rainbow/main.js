5276dde782e7bff9a2509ff718399546213306ab
Tenshiboy <answer_stas@mail.ru>
2015-01-27 07:08:32 +0300

    Удалил хуйню

--- Rainbow/main.js
+++ Rainbow/main.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 
 // Creates a new 'main' state that wil contain the game
 var main_state = {
-//asd
+
     preload: function() { 
 
     this.game.stage.backgroundColor = '#71c5cf';
